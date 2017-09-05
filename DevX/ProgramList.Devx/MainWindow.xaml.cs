648ed5cc92117e4d57b566ec156280762b0b4c48
Rajesh Aravapalli <rajeshaz09@outlook.com>
2017-09-05 15:29:41 +0530

    devx initial

---
+++ DevX/ProgramList.Devx/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Core;
+
+namespace ProgramList.DevX
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DXWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadGrid_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClearAndLoadGrid_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
