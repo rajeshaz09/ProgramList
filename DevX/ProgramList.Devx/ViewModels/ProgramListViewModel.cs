6074610b65fe35829197fb0a70237be6158ed3a2
p-padhi <31234504+p-padhi@users.noreply.github.com>
2017-09-22 16:02:33 +0530

    showsearchpanel

--- DevX/ProgramList.Devx/ViewModels/ProgramListViewModel.cs
+++ DevX/ProgramList.Devx/ViewModels/ProgramListViewModel.cs
@@ -17,68 +17,72 @@ using System.Windows.Input;
 
 namespace ProgramList.DevX.ViewModels
 {
-    public class ProgramListViewModel: ProgramListViewModelBase
-    {
-        public ObservableCollectionCore<object> GridData { get; private set; }
+	public class ProgramListViewModel : ProgramListViewModelBase
+	{
+		public ObservableCollectionCore<object> GridData { get; private set; }
 
-        private ListItemBase _currentRow;
-        public ListItemBase CurrentRow
-        {
-            get { return _currentRow; }
-            set { SetProperty(ref _currentRow, value); }
-        }
+		private ListItemBase _currentRow;
+		public ListItemBase CurrentRow
+		{
+			get { return _currentRow; }
+			set { SetProperty(ref _currentRow, value); }
+		}
 
-        private ColumnBase _currentColumn;
-        public ColumnBase CurrentColumn
-        {
-            get { return _currentColumn; }
-            set { SetProperty(ref _currentColumn, value); }
-        }
-        #region Commands
-        public ICommand PreviewKeyDownCommand { get; private set; }
-        public ICommand CurrentColumnChangedCommand { get; private set; }
-        public ICommand CurrentRowChangedCommand { get; private set; }
-        #endregion Commands
+		private ColumnBase _currentColumn;
+		public ColumnBase CurrentColumn
+		{
+			get { return _currentColumn; }
+			set { SetProperty(ref _currentColumn, value); }
+		}
+		#region Commands
+		public ICommand PreviewKeyDownCommand { get; private set; }
+		public ICommand CurrentColumnChangedCommand { get; private set; }
+		public ICommand CurrentRowChangedCommand { get; private set; }
+		#endregion Commands
 
-        public ProgramListViewModel(int rows, int columnSets): base(rows, columnSets)
-        {
-            PreviewKeyDownCommand = new DelegateCommand<KeyEventArgs>(OnPreviewKeyDownHandler);
-            CurrentRowChangedCommand = new DelegateCommand<CurrentItemChangedEventArgs  >(OnCurrentRowChangedHandler);
-            CurrentColumnChangedCommand = new DelegateCommand<CurrentColumnChangedEventArgs>(OnCurrentColumnChangedHandler);
-            GridData = new ObservableCollectionCore<object>();
-        }
+		public ProgramListViewModel(int rows, int columnSets) : base(rows, columnSets)
+		{
+			PreviewKeyDownCommand = new DelegateCommand<KeyEventArgs>(OnPreviewKeyDownHandler);
+			CurrentRowChangedCommand = new DelegateCommand<CurrentItemChangedEventArgs>(OnCurrentRowChangedHandler);
+			CurrentColumnChangedCommand = new DelegateCommand<CurrentColumnChangedEventArgs>(OnCurrentColumnChangedHandler);
+			GridData = new ObservableCollectionCore<object>();
+		}
 
-        private void OnCurrentRowChangedHandler(CurrentItemChangedEventArgs e)
-        {
-            Trace.WriteLine($"Row Changed  {_currentRow.RowNumber}");
-        }
+		private void OnCurrentRowChangedHandler(CurrentItemChangedEventArgs e)
+		{
+			Trace.WriteLine($"Row Changed  {_currentRow.RowNumber}");
+		}
 
-        private void OnCurrentColumnChangedHandler(CurrentColumnChangedEventArgs e)
-        {
-            Trace.WriteLine($"Column Changed  {_currentColumn.FieldName}");
-        }
+		private void OnCurrentColumnChangedHandler(CurrentColumnChangedEventArgs e)
+		{
+			Trace.WriteLine($"Column Changed  {_currentColumn.FieldName}");
+		}
 
-        public ICommand GenerateCommand(string columnName)
-        {
-            var command = new DelegateCommand<ListItemBase>(
-                (model)=> 
-                {
-                    OnCommandExecute(columnName, model);
-                });
+		public ICommand GenerateCommand(string columnName)
+		{
+			var command = new DelegateCommand<ListItemBase>(
+					(model) =>
+					{
+						OnCommandExecute(columnName, model);
+					});
 
-            Commands.Add(columnName, command);
-            return command;
-        }
+			Commands.Add(columnName, command);
+			return command;
+		}
 
-        private static void OnCommandExecute(string columnName, ListItemBase model)
-        {
-            MessageBox.Show($"Row\t{model.RowNumber}{Environment.NewLine}Column\t{columnName}");
-        }
+		private static void OnCommandExecute(string columnName, ListItemBase model)
+		{
+			MessageBox.Show($"Row\t{model.RowNumber}{Environment.NewLine}Column\t{columnName}");
+		}
 
-        private void OnPreviewKeyDownHandler(KeyEventArgs e)
-        {
-        }
+		private void OnPreviewKeyDownHandler(KeyEventArgs e)
+		{
+			if (e.Key == Key.F && e.KeyboardDevice.Modifiers == ModifierKeys.Control)
+			{
+				e.Handled = true;
+			}
+		}
 
-    }
+	}
 }
 
--- DevX/ProgramList.Devx/Views/MyTableView.cs
+++ DevX/ProgramList.Devx/Views/MyTableView.cs
@@ -12,102 +12,103 @@ using DevExpress.Data;
 
 namespace ProgramList.DevX.Views
 {
-    public class MyTableView : TableView
-    {
-        private void ApplyIsSelected(bool isSelected, string columnName)
-        {
-            var viewModel = Grid.DataContext as ProgramListViewModel;
-            foreach (var row in viewModel.GridData)
-            {
-                var model = row as ListItemBase;
-                model.SetIsCurrentInternal(isSelected, $"IsCurrent_{columnName}");
-            }
-        }
+	public class MyTableView : TableView
+	{
+		private void ApplyIsSelected(bool isSelected, string columnName)
+		{
+			var viewModel = Grid.DataContext as ProgramListViewModel;
+			foreach (var row in viewModel.GridData)
+			{
+				var model = row as ListItemBase;
+				model.SetIsCurrentInternal(isSelected, $"IsCurrent_{columnName}");
+			}
+		}
 
-        protected override void OnColumnHeaderClick(ColumnBase column, bool isShift, bool isCtrl)
-        {
-            var columnInfo = column as Columns.ColumnInfo;
+		protected override void OnColumnHeaderClick(ColumnBase column, bool isShift, bool isCtrl)
+		{
+			var columnInfo = column as Columns.ColumnInfo;
 
-            if (isCtrl)
-            {
-                columnInfo.IsSelected = !columnInfo.IsSelected;
+			if (isCtrl)
+			{
+				columnInfo.IsSelected = !columnInfo.IsSelected;
 
-                ApplyIsSelected(columnInfo.IsSelected, columnInfo.FieldName);
-                return;
-            }
-            base.OnColumnHeaderClick(column, isShift, isCtrl);
-        }
+				ApplyIsSelected(columnInfo.IsSelected, columnInfo.FieldName);
+				return;
+			}
+			base.OnColumnHeaderClick(column, isShift, isCtrl);
+		}
 
-        protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
-        {
-            TableViewHitInfo hitInfo = CalcHitInfo(e.OriginalSource as DependencyObject);
-            if (hitInfo.InRowCell)
-            {
-                foreach (var columnInfo in Grid.Columns.Cast<Columns.ColumnInfo>())
-                {
-                    if (columnInfo.IsSelected)
-                    {
-                        columnInfo.IsSelected = false;
-                        ApplyIsSelected(false, columnInfo.FieldName);
-                    }
-                }
-            }
+		protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+		{
+			TableViewHitInfo hitInfo = CalcHitInfo(e.OriginalSource as DependencyObject);
+			if (hitInfo.InRowCell)
+			{
+				foreach (var columnInfo in Grid.Columns.Cast<Columns.ColumnInfo>())
+				{
+					if (columnInfo.IsSelected)
+					{
+						columnInfo.IsSelected = false;
+						ApplyIsSelected(false, columnInfo.FieldName);
+					}
+				}
+			}
 
-            base.OnMouseLeftButtonUp(e);
-        }
+			base.OnMouseLeftButtonUp(e);
+		}
 
-        protected override void OnKeyUp(KeyEventArgs e)
-        {
-            if (e.Key == Key.M && e.KeyboardDevice.Modifiers == ModifierKeys.Control)
-            {
-                AllowEditing = true;
-                foreach (var column in Grid.Columns.Cast<Columns.ColumnInfo>())
-                {
-                    //if (!column.ReadOnly)
-                    //    column.AllowEditing = DevExpress.Utils.DefaultBoolean.True;
-                    column.TabStop = !column.ReadOnly;
-                    column.AllowFocus = !column.ReadOnly;
+		protected override void OnKeyUp(KeyEventArgs e)
+		{
+			if (e.Key == Key.M && e.KeyboardDevice.Modifiers == ModifierKeys.Control)
+			{
+				AllowEditing = true;
+				foreach (var column in Grid.Columns.Cast<Columns.ColumnInfo>())
+				{
+					//if (!column.ReadOnly)
+					//    column.AllowEditing = DevExpress.Utils.DefaultBoolean.True;
+					column.TabStop = !column.ReadOnly;
+					column.AllowFocus = !column.ReadOnly;
 
-                    Commands.EditFocusedRow?.Execute(null);
-                }
+					Commands.EditFocusedRow?.Execute(null);
+				}
 
-                e.Handled = true;
-            }
-            else if (e.Key == Key.Q && e.KeyboardDevice.Modifiers == ModifierKeys.Control)
-            {
-                AllowEditing = false;
-                foreach (var column in Grid.Columns.Cast<Columns.ColumnInfo>())
-                {
-                    //if (!column.ReadOnly)
-                    //    column.AllowEditing = DevExpress.Utils.DefaultBoolean.False;
-                    column.TabStop = true;
-                    column.AllowFocus = true;
-                }
-                Commands.EndEditFocusedRow?.Execute(null);
-                //CommitEditing();
+				e.Handled = true;
+			}
+			else if (e.Key == Key.Q && e.KeyboardDevice.Modifiers == ModifierKeys.Control)
+			{
+				AllowEditing = false;
+				foreach (var column in Grid.Columns.Cast<Columns.ColumnInfo>())
+				{
+					//if (!column.ReadOnly)
+					//    column.AllowEditing = DevExpress.Utils.DefaultBoolean.False;
+					column.TabStop = true;
+					column.AllowFocus = true;
+				}
+				Commands.EndEditFocusedRow?.Execute(null);
+				//CommitEditing();
 
-                e.Handled = true;
-            }
-            else if (e.Key == Key.F && e.KeyboardDevice.Modifiers == ModifierKeys.Control)
-            {
-                e.Handled = false;
-            }
-            else if (e.Key == Key.N && e.KeyboardDevice.Modifiers == ModifierKeys.Control)
-            {
-                e.Handled = false;
-            }
-            else if (e.Key == Key.S && e.KeyboardDevice.Modifiers == ModifierKeys.Control)
-            {
-                e.Handled = false;
-            }
-            else if (e.Key == Key.F && e.KeyboardDevice.Modifiers == ModifierKeys.Shift)
-            {
+				e.Handled = true;
+			}
+			else if (e.Key == Key.F && e.KeyboardDevice.Modifiers == ModifierKeys.Control)
+			{
+				e.Handled = false;
+			}
+			else if (e.Key == Key.N && e.KeyboardDevice.Modifiers == ModifierKeys.Control)
+			{
+				e.Handled = false;
+			}
+			else if (e.Key == Key.S && e.KeyboardDevice.Modifiers == ModifierKeys.Control)
+			{
+				e.Handled = false;
+			}
+			else if (e.Key == Key.F && e.KeyboardDevice.Modifiers == ModifierKeys.Shift)
+			{
+				ShowSearchPanel(true);
 
-            }
-            else
+			}
+			else
 
-                base.OnKeyUp(e);
-        }
-        
-    }
+				base.OnKeyUp(e);
+		}
+
+	}
 }
